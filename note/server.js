43231c7aea97e3fef34b904f6ff05aa4746ac06a
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-10-08 09:59:32 +0100

    Make router

--- note/server.js
+++ note/server.js
@@ -1,1 +1,11 @@
-console.log("working well two")
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 8000
+
+
+app.use("/api/notes", require("./routes/noteRoutes"))
+
+app.listen(port, () => {
+    console.log('listening on port ' + port);
+})
