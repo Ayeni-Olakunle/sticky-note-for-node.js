1845feca7f63fb9d2a1df5f3844ca802cab0106f
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-10-08 11:17:49 +0100

    connect to mongoDB

--- note/server.js
+++ note/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const colors = require('colors')
 const port = process.env.PORT || 8000
 const { errorHandler } = require("./middleware/errorMiddleWare")
+const connectDB = require("./config/db")
 const app = express()
 
 
+connectDB()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
